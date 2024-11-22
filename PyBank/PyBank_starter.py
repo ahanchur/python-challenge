47d56bcb07d573a7b80aba16d039eb84ae1300cd
Akash Hanchur <140855932+ahanchur@users.noreply.github.com>
2024-11-21 20:44:27 -0500

    Fixing up the comments

--- PyBank/PyBank_starter.py
+++ PyBank/PyBank_starter.py
@@ -11,8 +11,6 @@ file_to_output = os.path.join("PyBank","analysis", "budget_analysis.txt")  # Out
 
 # Define variables to track the financial data
 total_months = 0
-total_net = 0
-cntr=0
 greatest_increase=0
 greatest_decrease=0
 dates=[]
@@ -21,8 +19,8 @@ difference=[]
 counter=0
 i_date=""
 d_date=""
+total_profit=0
 
-# Add more variables to track other necessary financial data
 
 # Open and read the csv
 with open(file_to_load) as financial_data:
@@ -33,46 +31,34 @@ with open(file_to_load) as financial_data:
 
     # Extract first row to avoid appending to net_change_list
     
-    # Track the total and net change
+    # Read the values and save them in seperate lists
     for row in reader:
         dates.append(row[0])
         outcome.append(int(row[1]))
     
-greatest_increase=outcome[0]
-greatest_decrease=outcome[0]
-total_profit=0
-
-for values in outcome:
-    counter+=1
     
-    if greatest_increase < values:
-        greatest_increase=values
-        i_count=counter
-        
-        
-    if greatest_decrease>values:
-        greatest_decrease=values
-        d_count=counter
-        
+
+# Calculate the total profit along with the total months
+for values in outcome:    
     total_profit=total_profit+values
     total_months+=1
 
     
-# Calculate the average net change across the months
+# Calculate the total difference and saving the differences in a list for further use
 total_difference=0
 for i in range(1,len(outcome)):
     total_difference=total_difference+(outcome[i]-outcome[i-1])
     difference.append(outcome[i]-outcome[i-1])
     
-    
+# Calculating and rounding the average change
 avg_change=total_difference/len(difference)
 avg_change=round(avg_change,2)
 
+# Setting the increase and decrease values to the first values so the loop can work properly
 greatest_increase=difference[0]
 greatest_decrease=difference[0]
 
-
-
+# Calculating the greatest increase and the mathing date
 for i in range (len(difference)):
     if difference[i]>greatest_increase:
         greatest_increase=difference[i]
--- PyPoll/PyPoll_starter.py
+++ PyPoll/PyPoll_starter.py
@@ -5,11 +5,12 @@
 import csv
 import os
 
+# Files to load and output (update with correct file paths)
 file_to_load = os.path.join("PyPoll", "Resources", "election_data.csv")
 file_to_output = os.path.join("PyPoll", "analysis", "election_analysis.txt")
 
+# Define variables to track the polling data
 total_votes = 0  
-
 candidates=[]
 vote_counts={}
 cur_candidate=""
@@ -27,14 +28,11 @@ with open(file_to_load) as election_data:
     
     
     
-    # Loop through each row of the dataset and process it
+    # Count the total votes, and the number of candidates
     for row in reader:
 
-        # Print a loading indicator (for large datasets)
-        #print(". ", end="")
-
-        total_votes=total_votes+1
         
+        total_votes=total_votes+1
         if cntr==0:
             cur_candidate=row[2]
             candidates.append(cur_candidate)
@@ -46,45 +44,37 @@ with open(file_to_load) as election_data:
                 
     vote_counts = {candidate: 0 for candidate in candidates}
     
+    
+#looping through the csv again to add the votes to the dictionary
 with open(file_to_load) as election_data:
     reader = csv.reader(election_data)
 
+    #skip for header
     header = next(reader)
         
+    #Add the votes to the value aspect of the dictionary if the name matches up with the key
     for row in reader:
         new_nominee = row[2]
         if new_nominee in vote_counts:
             vote_counts[new_nominee]+=1             
     
+    #saving the keys and values in seperate lists
     x=list(vote_counts.keys())
     y=list(vote_counts.values())  
       
+    #calcualting the percentage of votes each candidate got
     for i in y:
         percents.append(round(i/total_votes*100,3))
         
-        
+    #calculating the highest vote    
     top=y[0]
     for i in y:
         if top<i:
             top=i        
 
+    #searching for the name of the winner
     greatest= max(vote_counts,key=vote_counts.get)
     
-
-                
-                
-                
-                
-
-            
-            
-        
-            
-            
-       
-        
-
-        
 
 
 
@@ -96,7 +86,7 @@ with open(file_to_output, "w") as txt_file:
     txt_file.write("Total Votes: " + str(total_votes)+"\n")
     txt_file.write("-------------------------\n")
 
-    # Loop through the candidates to determine vote percentages and identify the winner
+    # Loop through the candidates to print all of their information
     for i in range(len(candidates)):
         txt_file.write(x[i]+": "+str(percents[i])+"% ("+str(y[i])+")\n")
     txt_file.write("-------------------------\n")
@@ -106,13 +96,7 @@ with open(file_to_output, "w") as txt_file:
         
         
 
-    # Get the vote count and calculate the percentage
-
-
-    # Update the winning candidate if this one has more votes
-
-
-    # Print and save each candidate's vote count and percentage
+  
 
 
     
